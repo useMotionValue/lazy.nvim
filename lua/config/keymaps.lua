8ba1bb4665cb2551ef5a56112f644b889232c03d
kaicong.he <kaicong.he@narwal.com>
2023-04-17 17:31:30 +0800

    initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local keymap = vim.keymap.set
+
+keymap("i", "jj", "<Esc>")
+keymap("i", "jk", "<Esc>:w<Enter>")
