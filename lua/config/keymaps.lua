12c9b0a556831448421634daeea5b42037ab3b2f
kaicong.he <kaicong.he@narwal.com>
2023-04-18 11:05:03 +0800

    windows mapping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,12 @@ local keymap = vim.keymap.set
 
 keymap("i", "jj", "<Esc>")
 keymap("i", "jk", "<Esc>:w<Enter>")
+keymap("n", "<C-a>", "gg<S-v>G")
+
+-- windows
+keymap("n", "<leader>ww", "<C-W>p", { desc = "Other window" })
+keymap("n", "<leader>wd", "<C-W>c", { desc = "Delete window" })
+keymap("n", "<leader>w-", "<C-W>s", { desc = "Split window below" })
+keymap("n", "<leader>w=", "<C-W>v", { desc = "Split window right" })
+keymap("n", "<leader>-", "<C-W>s", { desc = "Split window below" })
+keymap("n", "<leader>=", "<C-W>v", { desc = "Split window right" })
